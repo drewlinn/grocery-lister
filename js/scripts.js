c2002baf56ed0ac4c9c9f740702d759a827855f2
Andrew Dalton and monique st laurent <andrew@>
2017-03-01 20:56:21 -0800

    add alphabetizing and upper case function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     var items = ["item1","item2","item3","item4","item5","item6"];
+    // var sortedItems = items.sort()
+    // var ucSortedItems = sortedItems.toUpperCase()
 
-    items.forEach(function(item){
-      var userInput = $("input#" + item).val();
-      $("ul").append("<li>" + userInput + "</li>");
+    // items.forEach(function(item){
+    //   var userInput = $("input#" + item).val();
+    //   $("ul").append("<li>" + userInput + "</li>");
+    // });
+
+    var otherItems = items.map(function(item){
+      return $("input#" + item).val().toUpperCase();
     });
+
+    otherItems.sort().forEach(function(eachItem){
+      $("#list").append("<li>" + eachItem + "</li>");
+    });
+
 
     $(".list").show();
     $("form").hide();
